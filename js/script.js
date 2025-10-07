f0d3f482fd7ddd5550280056aa0dcc05b5b8b82c
alex <alexandru.cavaler2003@gmail.com>
2025-10-07 15:01:58 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-console.log("Site-ul Citadin Consulting este funcțional ✅");
-
 // Încarcă header-ul
 fetch('header.html')
   .then(response => response.text())
@@ -12,4 +10,22 @@ fetch('footer.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('footer-placeholder').innerHTML = data;
+
+    const backToTopBtn = document.getElementById("backToTopBtn");
+
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > 300) {
+      backToTopBtn.classList.add("show");
+    } else {
+      backToTopBtn.classList.remove("show");
+    }
   });
+
+  backToTopBtn.addEventListener("click", function() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+});
+
